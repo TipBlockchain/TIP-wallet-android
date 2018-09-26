869ad49fce3732860f2e9ab1c2cf310c33c70eb2
John Warmann <john.warmann@gmail.com>
2018-09-25 21:24:49 -0400

    Placeholder data to work with new data formats

--- app/src/main/java/io/tipblockchain/kasakasa/ui/mainapp/dummy/TransactionsContentManager.kt
+++ app/src/main/java/io/tipblockchain/kasakasa/ui/mainapp/dummy/TransactionsContentManager.kt
@@ -32,8 +32,8 @@ object TransactionsContentManager {
     private fun createTransactions() {
         val users: Array<User> = ContactsContentManager.ITEMS.toTypedArray()
         var transactions: Array<Transaction> = arrayOf(
-                Transaction(id = "1", hash = "0x123", from = users[5], to = users[11], currency = "TIP", value = BigInteger("122070"), time = Date(), message = "Pizza and drinks"),
-                Transaction(id = "2", hash = "0x124", from = users[9], to = users[4], currency = "TIP", value = BigInteger("48202"), time = Date(), message = "Bingo")
+                Transaction(id = "1", hash = "0x123", from = users[5].username, to = users[11].username, currency = "TIP", value = BigInteger("122070"), time = Date(), message = "Pizza and drinks"),
+                Transaction(id = "2", hash = "0x124", from = users[9].username, to = users[4].username, currency = "TIP", value = BigInteger("48202"), time = Date(), message = "Bingo")
 
         )
 
