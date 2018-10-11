f8cf13249b6e2e3905678b33acf89ae535a9e88c
John Warmann <john.warmann@gmail.com>
2018-10-11 02:03:08 -0400

    Added current and invalid properties to User and preferences

--- app/src/main/java/io/tipblockchain/kasakasa/app/Preferences.kt
+++ app/src/main/java/io/tipblockchain/kasakasa/app/Preferences.kt
@@ -4,13 +4,25 @@ import android.content.Context
 import android.content.SharedPreferences
 
 class Preferences (private var context: Context) {
-    private val PREFS_FILENAME = "io.tipblockchain.kasakasa.preferences"
 
-    private val ONBOARDING_COMPLETE = "onboarding_complete"
+    companion object {
 
-    private var prefs: SharedPreferences = context.getSharedPreferences(PREFS_FILENAME, 0)
+        private var instance: Preferences = Preferences(App.applicationContext())
 
-    var onboardingComplete: Boolean
-        get() = prefs.getBoolean(ONBOARDING_COMPLETE, false)
-        set(value) = prefs.edit().putBoolean(ONBOARDING_COMPLETE, value).apply()
+        private val PREFS_FILENAME = "io.tipblockchain.kasakasa.preferences"
+        private var prefs: SharedPreferences = instance.context.getSharedPreferences(PREFS_FILENAME, 0)
+
+        private val ONBOARDING_COMPLETE = "onboarding_complete"
+        private val CURRENT_USER = "current_user"
+
+
+        var onboardingComplete: Boolean
+            get() = prefs.getBoolean(ONBOARDING_COMPLETE, false)
+            set(value) = prefs.edit().putBoolean(ONBOARDING_COMPLETE, value).apply()
+
+        var currentUser: String?
+            get() = prefs.getString(CURRENT_USER, null)
+            set(value) = prefs.edit().putString(CURRENT_USER, value).apply()
+    }
+
 }
--- app/src/main/java/io/tipblockchain/kasakasa/data/db/entity/User.kt
+++ app/src/main/java/io/tipblockchain/kasakasa/data/db/entity/User.kt
@@ -1,9 +1,8 @@
 package io.tipblockchain.kasakasa.data.db.entity
 
-import android.arch.persistence.room.ColumnInfo
-import android.arch.persistence.room.Entity
-import android.arch.persistence.room.Index
-import android.arch.persistence.room.PrimaryKey
+import android.arch.persistence.room.*
+import io.tipblockchain.kasakasa.app.Preferences
+import kotlinx.serialization.json.JSON
 import java.util.*
 import javax.annotation.Nonnull
 
@@ -13,9 +12,40 @@ data class User(
         @ColumnInfo(name = "name") var name: String,
         @ColumnInfo(name = "username") var username: String,
         @ColumnInfo(name = "address") var address: String,
-        @ColumnInfo(name = "pictureUrl") var pictureUrl: String?,
+        @ColumnInfo(name = "pictureUrl") var pictureUrl: String? = null,
         @ColumnInfo(name = "isContact") var isContact: Boolean = false,
         @ColumnInfo(name = "isBlocked") var isBlocked: Boolean = false,
         @ColumnInfo(name = "lastMessage") var lastMessage: Date? = null
     ) {
+
+    @Ignore
+    var isValid: () -> Boolean = {
+        !(this.username.isEmpty() || this.id.isEmpty() || this.address.isEmpty())
+    }
+
+    companion object {
+        var current: User? = null
+            set(_) { save() }
+
+        fun invalid() : User =  User("", "", "", "")
+
+        fun fetch(): User? {
+            if (current == null) {
+                val userJson = Preferences.currentUser
+                if (userJson != null) {
+                    synchronized(User::class.java) {
+                        if (current == null) {
+                            current = JSON.parse(userJson)
+                        }
+                    }
+                }
+            }
+            return current
+        }
+
+        fun save() {
+            val userJson = JSON.stringify(this)
+            Preferences.currentUser = userJson
+        }
+    }
 }
