ca6f253e7701ff93e9cfb94829c3c678947c843e
John Warmann <john.warmann@gmail.com>
2018-10-15 20:52:11 -0400

    Replaced referenes to Preferences

--- app/src/main/java/io/tipblockchain/kasakasa/data/db/repository/UserRepository.kt
+++ app/src/main/java/io/tipblockchain/kasakasa/data/db/repository/UserRepository.kt
@@ -2,13 +2,11 @@ package io.tipblockchain.kasakasa.data.db.repository
 
 import android.app.Application
 import android.arch.lifecycle.LiveData
-import android.content.Context
 import android.os.AsyncTask
-import io.tipblockchain.kasakasa.app.Preferences
+import io.tipblockchain.kasakasa.app.PreferenceHelper
 import io.tipblockchain.kasakasa.data.db.TipRoomDatabase
 import io.tipblockchain.kasakasa.data.db.entity.User
 import io.tipblockchain.kasakasa.data.db.dao.UserDao
-import kotlinx.serialization.Serializer
 import kotlinx.serialization.json.JSON
 
 class UserRepository {
@@ -60,7 +58,7 @@ class UserRepository {
         private fun fetchCurrentUser(): User? {
             if (currentUser == null) {
                 synchronized(User::class.java) {
-                    val userJson = Preferences.currentUser
+                    val userJson = PreferenceHelper.currentUser
                     if (userJson != null) {
                         if (currentUser == null) {
                             currentUser = JSON.parse(userJson)
@@ -74,9 +72,9 @@ class UserRepository {
         private fun saveCurrentUser() {
             if (currentUser != null) {
                 val userJson = JSON.stringify(currentUser!!)
-                Preferences.currentUser = userJson
+                PreferenceHelper.currentUser = userJson
             } else {
-                Preferences.currentUser = null
+                PreferenceHelper.currentUser = null
             }
         }
     }
