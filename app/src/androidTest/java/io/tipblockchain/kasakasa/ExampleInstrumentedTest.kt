4d4468209120a5e36f7cf1a380f4b8932871eb34
John Warmann <john.warmann@gmail.com>
2018-10-27 16:18:53 -0400

    Update package name

--- app/src/androidTest/java/io/tipblockchain/kasakasa/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/io/tipblockchain/kasakasa/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("io.tipblockchain.kasakasaprototype", appContext.packageName)
+        assertEquals("io.tipblockchain.kasakasa", appContext.packageName)
     }
 }
